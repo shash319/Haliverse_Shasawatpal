a9e94f7d9e4c45bb1b62f9c409778f10625f65f7
Shasawat Pal <shashwatpal2001@gmail.com>
2023-12-06 17:39:59 +0530

    backgroundC

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+const header={
+    color: '#E75480',
+    backgroundColor: 'purple',
+    shadowColor:'green',
+    textAlign: 'center',
+    margin: '0px',
+    padding: '17px',
+};
+
+    return (  
+    <div >
+        <h1 style={header}>Heliverse</h1>
+    </div>
+    );
+}
+
+export default Header
+
+   
